bc81f7e8d5c7a80a8602621db14adabdd0fb9e20
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2023-12-21 19:45:56 -0600

    added intial javascript code

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('button').addEventListener('click',ageCalculation)
+
+function ageCalculation(){
+const inputDate = ""
+const currentDate = ""
+
+return currentDate - inputDate
+}
