1bb0297bcc172dac4735bbc92b9522d5b3c8922d
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-02-21 19:59:36 -0600

    added JS code

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-document.querySelector('button').addEventListener('click',ageCalculation)
+const button = document.querySelector('button')
+button.addEventListener('click',ageCalculation)
 
 function ageCalculation(){
-const inputDate = ""
-const currentDate = ""
+const inputDate = document.querySelector('input').value
+const date = new Date();
 
-return currentDate - inputDate
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+// This arrangement can be altered based on how we want the date's format to appear.
+let currentDate = `${day}-${month}-${year}`;
+console.log(currentDate); // "17-6-2022"
+
 }
+
+//You need the date input from the user
+//You need the current correct date
+//You need to compare the two and derive the age based on the distance between the two
+//Take this value and put that back in the DOM
