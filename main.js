cf8e97602dfab20ebafe629270968689cc094159
Gazerbeam <134249845+Gazerbeam2@users.noreply.github.com>
2024-02-21 23:32:39 -0600

    added changes

--- main.js
+++ main.js
@@ -14,18 +14,6 @@ console.log(inputDate);
 
 //return currentDate - inputDate
 }
-const inputDate = document.querySelector('input').value
-const date = new Date();
-
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-
-// This arrangement can be altered based on how we want the date's format to appear.
-let currentDate = `${day}-${month}-${year}`;
-console.log(currentDate); // "17-6-2022"
-
-}
 
 //You need the date input from the user
 //You need the current correct date
